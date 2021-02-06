2b703419f7dea66d310f80b8323eabc193cdfa91
Oleg <oleg3422@gmail.com>
2021-02-06 15:00:10 +0500

    +bat
    +welcome string
    +cycle while exit

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ def main_c(inp_str_):
     temp_arr = str_parse(inp_str_)
     return final(temp_arr)
 
-inp_str = input()
-print(main_c(inp_str))
+	
+exit = True	
+while exit:
+	print('Input string \n Input "exit" for exit')
+	inp_str = input()
+	if inp_str == 'exit':
+		exit = False
+		break
+	else:		
+		print(main_c(inp_str))
+		print('<-------------------------------->')
+
 
 
 
