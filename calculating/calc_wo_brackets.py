00b9dd343001a2170df68deb81039e7d609c24ca
Oleg <oleg3422@gmail.com>
2021-02-07 23:29:50 +0500

    +fixes for calculating

--- calculating/calc_wo_brackets.py
+++ calculating/calc_wo_brackets.py
@@ -2,7 +2,7 @@ from calculating.priority import priority
 from calculating.return_res import return_res
 
 def calculating_w_out_br(arr):
-  #print(arr, '  +  ')  
+  print(arr, '  +  ')  
   st1 = []
   st2 = ['']
   temp = 0
@@ -18,11 +18,11 @@ def calculating_w_out_br(arr):
       return 'input data error // incorrect symbols or wrong data'
   #elif len(arr)<3 or (arr[-1] in znaki) :
   #    return 'input data error // not enough data'
-  #print(arr)
+  print(arr,1)
   if len(arr) == 1:
         return arr[0]
   while temp < len(arr):
-    #print(st1,st2)
+    print(st1,st2,2)
     if st2 == []:
             break
     #print(temp)
@@ -32,27 +32,28 @@ def calculating_w_out_br(arr):
     elif arr[temp] not in znaki:
       st1.append(arr[temp])
       temp += 1
-      #print(st1,st2)
+      print(st1,st2,3)
 
     elif arr[temp] in znaki:
       if st2 == []:
             break
-      #print(st1,st2,temp)
+      print(st1,st2,4)
       if priority(arr[temp]) > priority(st2[len(st2)-1]):
         if 'zero division' in st1:
             st1[0] = 'zero division'
             return st1[0]
         if len(st1) > 1 and (priority(st2[len(st2)-1])==1 or priority(st2[len(st2)-1])==2 or priority(st2[len(st2)-1])==0):
+          #st1[0] = return_res(st2[0], st1[0], st1[1] )
           st1[len(st1)-2] = return_res(st2[len(st2)-1], st1[len(st1)-2], st1[len(st1)-1] )
           st1.pop()
         st2.pop()
         st2.append(arr[temp])
         temp += 1
       elif priority(arr[temp]) == priority(st2[len(st2)-1]):
-          if len(st1) > 1 and priority(st2[len(st2)-1])==2:
+          if len(st1) > 1 and (priority(st2[len(st2)-1])==1 or priority(st2[len(st2)-1])==2 or priority(st2[len(st2)-1])==0):
              st1[0] = return_res(st2[0], st1[0], st1[1] )
              st1.pop()   
-             #print(st1,st2,'-----------')
+             print(st1,st2,4)
           del st2[0]
           st2.append(arr[temp])
           temp += 1   
@@ -61,29 +62,39 @@ def calculating_w_out_br(arr):
         temp += 1 
     
 
-    #print(st1,st2,'++')
+    print(st1,st2,5)
     if st2 == []:
         break
     if temp == len(arr):
+      temp_ =  0   
       while st2 != []:
-        if '-' in st2:
-            if 'zero division' in st1:
-              st1[0] = 'zero division'
-              return st1[0]
-            if len(st1) == 1:
-              break
-            st1[0] = return_res(st2[0], st1[0], st1[1])      
-            #st1[len(st1)-2] = return_res(st2[len(st2)-1], st1[len(st1)-2], st1[len(st1)-1])            
-            del st1[1]
-            del st2[0]
-            #st1.pop()
-            #st2.pop()
+        # if '-' in st2:
+        #     if 'zero division' in st1:
+        #       st1[0] = 'zero division'
+        #       return st1[0]
+            # if len(st1) == 1:
+            #   break
+            
+            # # while temp_ <= len(st1)-1:                  
+            # #   if priority(st2[temp_]) <= priority(st2[temp_-1]):
+            # #     st1[0] = return_res(st2[0], st1[0], st1[1])
+            # #     temp_ += 1      
+            # #st1[len(st1)-2] = return_res(st2[len(st2)-1], st1[len(st1)-2], st1[len(st1)-1])            
+            # del st1[1]
+            # del st2[0]
+            # #st1.pop()
+            # #st2.pop()
             
 
-        else:
+        # else:
+          if len(st1) > 1 and (priority(st2[len(st2)-1])==1 or priority(st2[len(st2)-1])==2 or priority(st2[len(st2)-1])==0):
+             #st1[0] = return_res(st2[0], st1[0], st1[1] )
+            st1[len(st1)-2] = return_res(st2[len(st2)-1], st1[len(st1)-2], st1[len(st1)-1] )
+            st1.pop()
+          st2.pop()
           if st2 == []:
             break
-          #print(st1,st2)
+          print(st1,st2,6)
           if 'zero division' in st1:
             st1[0] = 'zero division'
             return st1[0]
@@ -97,7 +108,7 @@ def calculating_w_out_br(arr):
 
           #print(st1,st2,temp)
   #st1.pop('')
-  #print(st1, st2, '  --  ')
+  print(st1, st2, 7)
   try:
     if str(st1[0])[-2:] == '.0' :
       return int(st1[0])
